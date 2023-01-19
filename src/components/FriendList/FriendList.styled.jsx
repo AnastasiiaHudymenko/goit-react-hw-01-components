90f8001603d3f91b5e860849a149117b0abb2e44
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-01-19 16:41:19 +0200

    add FriendList copomemt

---
+++ src/components/FriendList/FriendList.styled.jsx
@@ -0,0 +1,6 @@
+// export const FriendList = styled.ul`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 25px;
+//   background-color: white;
+// `;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Profile from './components/Profile/Profile';
 import user from './components/Profile/user.json';
 import Statistics from './components/Statistics/Statistics';
 import data from './components/Statistics/data.json';
+import FriendList from 'components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         stats={user.stats}
       />
       <Statistics stats={data} title="Upload stats" />
+      <FriendList friends={friends} />
     </App>
   </React.StrictMode>
 );
