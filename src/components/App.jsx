67a784780f68925fed81e30a43b6ec3c043e1f6b
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-01-19 17:46:08 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,13 @@ export const App = ({ children }) => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+
         color: '#010101',
         gap: 50,
-        marginTop: 100,
       }}
     >
       {children}
