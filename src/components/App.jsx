b0ace339ff7a8d48d3b005016396cd57b1df0057
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-02-10 09:51:17 +0200

    fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,13 @@
-export const App = ({ children }) => {
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
+
+export const App = () => {
   return (
     <div
       style={{
@@ -11,7 +20,17 @@ export const App = ({ children }) => {
         gap: 50,
       }}
     >
-      {children}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} title="Upload stats" />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
